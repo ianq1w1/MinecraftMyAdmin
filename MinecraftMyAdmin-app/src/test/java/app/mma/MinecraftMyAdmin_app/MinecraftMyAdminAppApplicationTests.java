8fc6c06b922e7ca557d57f3b24012ad5cf056f81
ianq1w1 <ianpereiraian2@gmail.com>
2026-04-22 23:53:52 -0300

    initial archives and dependencies

---
+++ MinecraftMyAdmin-app/src/test/java/app/mma/MinecraftMyAdmin_app/MinecraftMyAdminAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mma.MinecraftMyAdmin_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftMyAdminAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
